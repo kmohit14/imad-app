0107c717d96945775dbc594481c4c8723ab7edf9
Mohit <31005636+kmohit14@users.noreply.github.com>
2017-08-16 10:52:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ element.innerHTML = 'Hello, I am Mohit K...!!';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 100);
 };
